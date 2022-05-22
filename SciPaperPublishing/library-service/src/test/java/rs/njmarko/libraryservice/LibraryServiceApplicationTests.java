a7bd89aeb1c5a760ea16a007a433d56eff190a95
njmarko <marko.njegomir@uns.ac.rs>
2022-05-22 18:37:17 +0200

    Added library service.

---
+++ SciPaperPublishing/library-service/src/test/java/rs/njmarko/libraryservice/LibraryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.njmarko.libraryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
